0d4941df61e8fe8a6d409b45c198e6ca5e2eae66
Tom MacWright <tom@macwright.org>
2013-08-05 15:43:42 -0400

    First

---
+++ geojsonio.js
@@ -0,0 +1,11 @@
+function getClassed(e, c) { return e.getElementsByClassName(c); }
+
+if (window.location.href.match(/\.geojson$/)) {
+    var meta = getClassed(document.body, 'meta')[0],
+        group = getClassed(meta, 'button-group')[0],
+        button = group.appendChild(document.createElement('a'));
+    button.innerHTML = 'geojson.io';
+    button.className = 'button minibutton';
+    button.setAttribute('_target', 'blank');
+    button.setAttribute('href', 'http://localhost:3000/#github:' + location.pathname);
+}
